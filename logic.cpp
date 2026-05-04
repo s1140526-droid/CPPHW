8f018feec6bca39e854b69080348bc23576b1700
s1140526-droid <s1140526@dmhs.kh.edu.tw>
2026-05-04 09:41:36 +0800

    Create  logic.cpp

---
+++ logic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 8;
+    int a = 3;
+    int a = a - b; 
+    cout << (a < b) <<"";
+    cout << (a >= b) <<"";
+    cout << (a >= c) <<"";
+    cout << (b < c) <<"";
+    cout << (b == b) <<"";
+    cout << (a != c) <<"";
+0    return 0;
+}
